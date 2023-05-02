a4a33f81cc152aca1d43a1c8f72b2e8cad07d616
yjy <wpdl7899>
2023-05-02 16:46:55 +0900

    스프링 기초 - 1

---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+//package hello.core.order;
+//
+//import hello.core.member.*;
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//
+//public class OrderServiceTest {
+//
+////    MemberService memberService = new MemberServiceImpl(MemberRepository memberRepository);
+//    OrderService orderService = new OrderServiceImpl();
+//
+//    @Test
+//    void  createOrder(){
+//        long memberId = 1L;
+//        //given
+//        Member member = new Member(memberId, "memberA", Grade.VIP);
+//
+//        // when
+//        memberService.join(member);
+//        Order order = orderService.createOrder(memberId, "itemA", 20000);
+//
+//        // then
+//        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+//    }
+//
+//
+//}
